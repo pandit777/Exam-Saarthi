061521bd41f9c8ddc4c8093cedd729e5df0bda2d
pandit777 <201600376+pandit777@users.noreply.github.com>
2026-01-06 22:48:44 -0800

    Update course-app.js

--- uor/course-app.js
+++ uor/course-app.js
@@ -93,7 +93,7 @@ headerContent.style.zIndex = '100';
 
 // Logo Area
 const logoArea = document.createElement('a');
-logoArea.href = 'https://pandit777.github.io/pyq/index.html';
+logoArea.href = 'https://examsaarthi.com/';
 logoArea.className = 'logo-area';
 logoArea.style.display = 'flex';
 logoArea.style.alignItems = 'center';
@@ -815,4 +815,5 @@ document.head.appendChild(googleFonts2);
 const interFont = document.createElement('link');
 interFont.rel = 'stylesheet';
 interFont.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap';
+
 document.head.appendChild(interFont);
