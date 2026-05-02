08e638a27999410960cee4301bf94b16ba4f50b6
Gourav Sharma <201600376+pandit777@users.noreply.github.com>
2026-05-02 21:07:26 +0530

    Update settings.py

--- loginproject/settings.py
+++ loginproject/settings.py
@@ -123,5 +123,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'gs8901346287@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'dihwoznvpwqambgo')
