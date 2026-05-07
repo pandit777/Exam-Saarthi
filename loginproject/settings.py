1f9b55fba29590baef0d7dab52ee89b21f55abf8
Gourav Sharma <201600376+pandit777@users.noreply.github.com>
2026-05-07 11:21:49 +0530

    Update settings.py

--- loginproject/settings.py
+++ loginproject/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'examsaarthi.com' 'www.examsaarthi.com '.onrender.com,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["examsaarthi.com","www.examsaarthi.com","exam-saarthi-4387.onrender.com","localhost","127.0.0.1",]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
